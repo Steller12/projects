5fd4dabd1fa6c773cf1c09f86a5fdbf6f229796f
Pushp Gupta <pugpta2601@gmail.com>
2022-10-09 21:26:06 +0530

    python rps game

---
+++ r-p-s.py
@@ -0,0 +1,41 @@
+#import choice from random module
+from random import choice
+
+#computer input
+pick=['R','P','S']
+computer_input=choice(pick)
+
+#user input
+user_input=input('Chose your move([R]OCK/[P]APER/[S]CISSOR):')
+
+#all possibilities
+if computer_input=='R' and user_input=='R':
+    print("It's a draw")
+
+elif computer_input=='P' and user_input=='P':
+    print("It's a draw")
+
+elif computer_input=='S' and user_input=='S':
+    print("It's a draw")
+
+elif computer_input=='R' and user_input=='S':
+    print("You loose")
+
+elif computer_input=='P' and user_input=='R':
+    print("You loose")
+
+elif computer_input=='S' and user_input=='P':
+    print("You loose")
+
+elif computer_input=='R' and user_input=='P':
+    print("You won the game")
+
+elif computer_input=='P' and user_input=='S':
+    print("You won the game")
+
+elif computer_input=='S' and user_input=='R':
+    print("You won the game")
+
+#if the input is invalid
+else:
+    print("Give a valid input")
