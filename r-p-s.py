a6367695d869ac9361a1712e8778429a57a0ebc5
Pushp Gupta <pugpta2601@gmail.com>
2022-10-11 20:49:31 +0530

    rps edit

--- r-p-s.py
+++ r-p-s.py
@@ -1,41 +1,45 @@
 #import choice from random module
 from random import choice
 
-#computer input
-pick=['R','P','S']
-computer_input=choice(pick)
+while True:
 
-#user input
-user_input=input('Chose your move([R]OCK/[P]APER/[S]CISSOR):')
+    #computer input
+    pick=['R','P','S']
+    computer_input=choice(pick)
 
-#all possibilities
-if computer_input=='R' and user_input=='R':
-    print("It's a draw")
+    #user input
+    user_input=input('Chose your move([R]OCK/[P]APER/[S]CISSOR):')
 
-elif computer_input=='P' and user_input=='P':
-    print("It's a draw")
+    #all possibilities
+    if computer_input=='R' and user_input=='R':
+        print("It's a draw")
 
-elif computer_input=='S' and user_input=='S':
-    print("It's a draw")
+    elif computer_input=='P' and user_input=='P':
+        print("It's a draw")
 
-elif computer_input=='R' and user_input=='S':
-    print("You loose")
+    elif computer_input=='S' and user_input=='S':
+        print("It's a draw")
 
-elif computer_input=='P' and user_input=='R':
-    print("You loose")
+    elif computer_input=='R' and user_input=='S':
+        print("You loose")
 
-elif computer_input=='S' and user_input=='P':
-    print("You loose")
+    elif computer_input=='P' and user_input=='R':
+        print("You loose")
 
-elif computer_input=='R' and user_input=='P':
-    print("You won the game")
+    elif computer_input=='S' and user_input=='P':
+        print("You loose")
 
-elif computer_input=='P' and user_input=='S':
-    print("You won the game")
+    elif computer_input=='R' and user_input=='P':
+        print("You won the game")
 
-elif computer_input=='S' and user_input=='R':
-    print("You won the game")
+    elif computer_input=='P' and user_input=='S':
+        print("You won the game")
 
-#if the input is invalid
-else:
-    print("Give a valid input")
+    elif computer_input=='S' and user_input=='R':
+        print("You won the game")
+    #if the input is invalid
+    else:
+        print("Give a valid input")
+    option=int(input("Enter 0 if you want to play again:"))
+    if (option):
+        break
