f248f27364459111ea398e58a787e477bcb140d9
Pushp Gupta <pugpta2601@gmail.com>
2022-10-15 15:02:55 +0530

    all projects

--- r-p-s.py
+++ r-p-s.py
@@ -12,31 +12,31 @@ while True:
 
     #all possibilities
     if computer_input=='R' and user_input=='R':
-        print("It's a draw")
+        print("It's a draw. computer chose rock")
 
     elif computer_input=='P' and user_input=='P':
-        print("It's a draw")
+        print("It's a draw. computer chose paper")
 
     elif computer_input=='S' and user_input=='S':
-        print("It's a draw")
+        print("It's a draw. computer chose scissors")
 
     elif computer_input=='R' and user_input=='S':
-        print("You loose")
+        print("You loose. computer chose rock")
 
     elif computer_input=='P' and user_input=='R':
-        print("You loose")
+        print("You loose. computer chose paper")
 
     elif computer_input=='S' and user_input=='P':
-        print("You loose")
+        print("You loose. computer chose scissors")
 
     elif computer_input=='R' and user_input=='P':
-        print("You won the game")
+        print("You won the game. computer chose rock")
 
     elif computer_input=='P' and user_input=='S':
-        print("You won the game")
+        print("You won the game. computer chose paper")
 
     elif computer_input=='S' and user_input=='R':
-        print("You won the game")
+        print("You won the game. computer chose scissors")
     #if the input is invalid
     else:
         print("Give a valid input")
