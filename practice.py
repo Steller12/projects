5b4bf8fc358432e631fe07bc120f5f1f14376ced
Pushp Gupta <pugpta2601@gmail.com>
2022-10-31 16:52:41 +0530

    edit

---
+++ practice.py
@@ -0,0 +1,18 @@
+#def func(x):
+ # res = 0
+#  for i in range(x):
+  #   res += i
+ # return res
+#print(func(4))
+#print(1)
+#raise ValueError
+#print(2)
+#try:
+#  print(1 / 0)
+#except ZeroDivisionError:
+#  raise ValueError
+try:
+    num= 5/0
+except:
+    print("an error occured!!")
+    raise
